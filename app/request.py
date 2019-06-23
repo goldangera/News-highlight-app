24701e5f95a251b8c126a9fa537287c7de14b56f
goldangera <nkirotegolda6888@gmail.com>
2019-06-23 21:28:18 +0300

    created a function to configure-request when the API is called

---
+++ app/request.py
@@ -0,0 +1,16 @@
+import json
+import urllib.request
+from .models import Sources,News
+
+# retrieving api key
+
+api_key = None
+
+# retrieving news base url
+
+base_url = None
+
+def configure_request(app):
+    global api_key, base_url
+    api_key = app.config["NEWS_API_KEY"]
+    base_url = app.config["NEWS_API_BASE_URL"]
